6b9cf404bb899fdcbf441c8eca2f2e9ea75bb94a
Dev Raj <devrajoff19@gmail.com>
2026-03-15 17:20:04 +0530

    UC8 linked list based palindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,25 +1,79 @@
-import java.util.Queue;
-import java.util.LinkedList;
-import java.util.Stack;
+class Node {
+    char data;
+    Node next;
+
+    Node(char data) {
+        this.data = data;
+        this.next = null;
+    }
+}
 
 public class PalindromeCheckerApp {
-    public static void main(String[] args) {
 
-        String str = "hello";
+    public static Node reverse(Node head) {
+        Node prev = null;
+        Node curr = head;
+        Node next;
 
-        Queue<Character> queue = new LinkedList<>();
-        Stack<Character> stack = new Stack<>();
-
-        // Enqueue characters and push into stack
-        for (int i = 0; i < str.length(); i++) {
-            queue.add(str.charAt(i));
-            stack.push(str.charAt(i));
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
         }
 
-        // Compare dequeue vs pop
-        while (!queue.isEmpty()) {
-            System.out.println("Queue (FIFO): " + queue.remove());
-            System.out.println("Stack (LIFO): " + stack.pop());
+        return prev;
+    }
+
+    public static boolean isPalindrome(Node head) {
+
+        if (head == null || head.next == null)
+            return true;
+
+        Node slow = head;
+        Node fast = head;
+
+        // Find middle
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Reverse second half
+        Node secondHalf = reverse(slow.next);
+
+        Node firstHalf = head;
+        Node temp = secondHalf;
+
+        // Compare halves
+        while (temp != null) {
+            if (firstHalf.data != temp.data) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            temp = temp.next;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String str = "madam";
+
+        Node head = new Node(str.charAt(0));
+        Node current = head;
+
+        // Convert string to linked list
+        for (int i = 1; i < str.length(); i++) {
+            current.next = new Node(str.charAt(i));
+            current = current.next;
+        }
+
+        if (isPalindrome(head)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
         }
     }
 }
